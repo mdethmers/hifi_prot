051bbf3eabf569f8ca21e9676a8560d28e57443a
Matthijs dethmers <i.m.dethmers2@student.utwente.nl>
2019-12-17 14:17:04 +0100

    Initial commit

---
+++ android/app/src/main/java/com/hifiprot/hifi_prot/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hifiprot.hifi_prot;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
